189ee2fabb1b55db70bceb76fc752060b247aa4d
meganthadev <meganthadev@gmail.com>
2020-08-12 20:02:08 -0700

    Refactor event listeners

--- mk-frontend/src/recipe.js
+++ mk-frontend/src/recipe.js
@@ -1,3 +1,5 @@
+const recipeEndPoint = "http://localhost:3000/api/v1/recipes"
+
 class Recipe {
 
     constructor(recipe, recipeAttributes) {
@@ -7,7 +9,11 @@ class Recipe {
         this.instructions = recipeAttributes.instructions
         this.category = recipeAttributes.category
         Recipe.all.push(this)
-    }
+    }  
+    
+    static findById(id) {
+        return this.all.find(recipe => recipe.id === id);
+      }
 
     renderRecipeCard() {
         return `
@@ -18,11 +24,61 @@ class Recipe {
                 </div>
                 <br><br>`; 
     }   
-
-    static findById(id) {
-        return this.all.find(recipe += recipe.id === id)
+  
     }
- 
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  getRecipes()
+
+    const createRecipeForm = document.querySelector('#create-recipe-form')
+    createRecipeForm.addEventListener("submit", (e) => createFormHandler(e))
+    //listen for 'click' event on recipe contain
+    const recipeContainer = document.querySelector('#recipe-container')
+    recipeContainer.addEventListener('click', e => {
+      const id = parseInt(e.target.dataset.id);
+      const recipe = Recipe.findById(id);
+     // document.querySelector('#recipe-container').innerHTML = recipe.findById(id);
+      console.log(recipe);
+    });
+  })
+
+function getRecipes() {
+      fetch(recipeEndPoint) 
+       .then(response => response.json())
+       .then(recipes => {
+           recipes.data.forEach(recipe => { //recipes.data regards to serializer, iterate over serialied data
+            let newRecipe =  new Recipe(recipe, recipe.attributes)  //passed recipe data to new instance of recipe class and construct obj w/ it
+            document.querySelector('#recipe-container').innerHTML += newRecipe.renderRecipeCard()
+           })
+         //  .catch(err => console.log(err))
+       })
+    }
+
+  function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const ingredientsInput = document.querySelector('#input-ingredients').value
+    const instructionsInput = document.querySelector('#input-instructions').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, ingredientsInput, instructionsInput, categoryId) 
+  }
+
+function postFetch(title, ingredients, instructions, category_id) {
+        const bodyData = {title, ingredients, instructions, category_id}
+        fetch(recipeEndPoint, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(bodyData)
+        })
+        .then(response => response.json()) 
+        .then(recipe => {
+            console.log(recipe);
+            const recipeData = recipe.data 
+            //render JSON response
+            let newRecipe =  new Recipe(recipeData, recipeData.attributes)
+                document.querySelector('#recipe-container').innerHTML += newRecipe.renderRecipeCard()
+        })
 
 }
 
